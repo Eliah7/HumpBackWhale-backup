b55f390d3977ca0688bc8d9cb9e637fbcfd0a8b9
Eliah7 <Guest@Eliahs-mac-2.local>
2018-05-22 15:09:38 +0300

    Adding Checkpoint to save the best model

--- training.py
+++ training.py
@@ -2,9 +2,15 @@
 from network import CNN
 import data_prep as dp
 import constants as c
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint
 
 # constants
 NB_CLASSES = 4251
+OPTIM = RMSprop()
+NB_EPOCHS = 20
+VALIDATION_SPLIT = 0.2
+VERBOSE = 0
 
 # data
 (X_train, y_train), (X_test, y_test) = dp.gen_training_data()
@@ -13,7 +19,12 @@ NB_CLASSES = 4251
 
 # training
 model = CNN.build(NB_CLASSES, c.IMG_ROWS, c.IMG_COLS, c.IMG_CHANNELS)
+model.compile(loss='categorical_crossentropy', optimizer=OPTIM, metrics=['accuracy'])
 
+checkpoint = ModelCheckpoint(filepath='model.h5', save_best_only=True)
+
+model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=NB_EPOCHS, validation_split=VALIDATION_SPLIT, verbose=VERBOSE, callbacks=[checkpoint])
+score = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE, verbose=VERBOSE)
 
 # TODO: Actually do the training
 # TODO: Save the model in a h5 file
